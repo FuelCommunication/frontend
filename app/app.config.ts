bd4f16ce63be4cab974ac527d41248b2e4629bfc
tokin_nikita <nikitatokin2006@gmail.com>
2026-04-19 12:00:00 +0300

    remove configs

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,10 +1,1 @@
-export default defineAppConfig({
-    apiUrl: "http://127.0.0.1:8000/",
-    ui: {
-        // button: {
-        //     defaultVariants: {
-        //         color: 'neutral'
-        //     }
-        // }
-    }
-});
+export default defineAppConfig({});
