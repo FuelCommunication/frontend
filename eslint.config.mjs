c937353a9a226e9a2bac72b4d4e39fcc8fd297e1
tokin_nikita <nikitatokin2006@gmail.com>
2026-04-19 12:00:00 +0300

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,15 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-    // your custom flat configs go here, for example:
-    // {
-    //   files: ['**/*.ts', '**/*.tsx'],
-    //   rules: {
-    //     'no-console': 'off' // allow console.log in TypeScript files
-    //   }
-    // },
-    // {
-    //   ...
-    // }
+    {
+        rules: {
+            'no-console': 'warn',
+            'prefer-const': 'error',
+            'no-undef': 'off',
+            'vue/no-unused-refs': 'warn',
+            'vue/no-v-html': 'warn',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+        },
+    },
 )
