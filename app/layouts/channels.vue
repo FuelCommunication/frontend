baaca21904e0ddb515e2ba6c0157d9ff6bd545ed
tokin_nikita <nikitatokin2006@gmail.com>
2026-04-19 12:00:00 +0300

    update channel layout

--- app/layouts/channels.vue
+++ app/layouts/channels.vue
@@ -1,22 +1,42 @@
+z
 <script setup lang="ts">
-import {LazyChannelCreateForm, LazySettingsModal} from "#components";
+import { LazyChannelCreateForm, LazySettingsModal } from "#components";
 
-const overlay = useOverlay()
-const localePath = useLocalePath()
-const modalChannelCreate = overlay.create(LazyChannelCreateForm)
-const modalSettings = overlay.create(LazySettingsModal)
+const overlay = useOverlay();
+const localePath = useLocalePath();
+const modalChannelCreate = overlay.create(LazyChannelCreateForm);
+const modalSettings = overlay.create(LazySettingsModal);
 </script>
 
 <template>
-  <div class="flex h-screen">
-    <ChannelSidebar>
-      <UButton class="rounded-xl cursor-pointer" size="xl" icon="i-lucide-plus" variant="solid" @click="modalChannelCreate.open()"/>
-      <UButton class="rounded-xl cursor-pointer" size="xl" icon="i-lucide-video" variant="ghost" :to="localePath('/calls')"/>
-      <UButton class="rounded-xl cursor-pointer" size="xl" icon="i-lucide-settings" variant="ghost" @click="modalSettings.open()"/>
-    </ChannelSidebar>
-    <UContainer class="lg:ml-20">
-      <slot/>
-    </UContainer>
-  </div>
+    <div class="flex h-screen">
+        <ChannelSidebar>
+            <UButton
+                class="rounded-xl cursor-pointer"
+                size="xl"
+                icon="i-lucide-plus"
+                variant="solid"
+                @click="modalChannelCreate.open()"
+            />
+            <UButton
+                class="rounded-xl cursor-pointer"
+                size="xl"
+                icon="i-lucide-video"
+                variant="ghost"
+                :to="localePath('/calls')"
+            />
+            <UButton
+                class="rounded-xl cursor-pointer"
+                size="xl"
+                icon="i-lucide-settings"
+                variant="ghost"
+                @click="modalSettings.open()"
+            />
+        </ChannelSidebar>
+        <main class="flex-1 lg:pl-20 overflow-y-auto">
+            <UContainer>
+                <slot />
+            </UContainer>
+        </main>
+    </div>
 </template>
-
