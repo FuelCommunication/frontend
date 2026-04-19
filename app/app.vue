950359196fb914e725840de3685ccad87c2126bd
tokin_nikita <nikitatokin2006@gmail.com>
2026-04-19 12:00:00 +0300

    fix locales

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,23 @@
 <script setup lang="ts">
 import * as locales from "@nuxt/ui/locale";
+import { en } from "@nuxt/ui/locale";
 
-const {locale} = useI18n();
+const { locale } = useI18n();
+
+useHead({
+    htmlAttrs: { lang: locale },
+});
+
+const uiLocale = computed<typeof en>(() => {
+    const list = locales as unknown as Record<string, typeof en>;
+    return list[locale.value] ?? en;
+});
 </script>
 
 <template>
-  <UApp :locale="locales[locale]">
-    <NuxtLayout>
-      <NuxtPage/>
-    </NuxtLayout>
-  </UApp>
+    <UApp :locale="uiLocale">
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+    </UApp>
 </template>
