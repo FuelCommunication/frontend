3cbde2f28753855ed6a8b1c61218eb341045da0f
tokin_nikita <nikitatokin2006@gmail.com>
2026-04-29 17:14:52 +0300

    update error.vue

--- app/error.vue
+++ app/error.vue
@@ -1,15 +1,30 @@
+<script setup lang="ts">
+import type {NuxtError} from "#app";
+
+const props = defineProps<{ error: NuxtError }>();
+const {t, te} = useI18n();
+
+const statusCode = computed(() => props.error.status ?? 500);
+
+const message = computed(() => {
+  const key = `error.${statusCode.value}`;
+  return te(key) ? t(key) : t("error.default");
+});
+</script>
+
 <template>
   <UError
       :clear="{
-      color: 'neutral',
-      size: 'xl',
-      icon: 'i-lucide-arrow-left',
-      class: 'rounded-full'
-    }"
+            color: 'neutral',
+            size: 'xl',
+            icon: 'i-lucide-arrow-left',
+            label: t('error.back'),
+            class: 'rounded-full',
+        }"
       :error="{
-      statusCode: 404,
-      statusMessage: 'Page not found',
-      message: 'The page you are looking for does not exist.'
-    }"
+            statusCode: statusCode,
+            statusMessage: props.error.statusText ?? t('error.default'),
+            message: message,
+        }"
   />
 </template>
